9508450c2bd4457ab8685c7feb2db07d0ebea128
John Wang <johncwang@gmail.com>
2019-10-25 04:50:09 -0700

    add more logging

--- controllers/handle_call_exit.go
+++ controllers/handle_call_exit.go
@@ -1,20 +1,38 @@
 package controllers
 
 import (
+	"io/ioutil"
 	"net/http"
+
+	log "github.com/sirupsen/logrus"
 )
 
 func HandleCallExit() func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
+		log.Info("EVT_RECEIVE__ON_CALL_EXIT")
 	}
 }
 
 func HandleCommandUpdate() func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
+		log.Info("EVT_RECEIVE__ON_COMMAND_UPDATE")
+		bytes, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			log.Warn(err.Error())
+		} else {
+			log.Info(string(bytes))
+		}
 	}
 }
 
 func HandleCommandError() func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
+		log.Info("EVT_RECEIVE__ON_COMMAND_ERROR")
+		bytes, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			log.Warn(err.Error())
+		} else {
+			log.Info(string(bytes))
+		}
 	}
 }
